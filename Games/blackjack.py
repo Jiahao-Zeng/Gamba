2586c1f702cfac5265d15c042a7909272213180c
Jiahao Zeng <157934567+Jiahao-Zeng@users.noreply.github.com>
2025-04-12 21:36:18 -0400

    Created deck

--- Games/blackjack.py
+++ Games/blackjack.py
@@ -1,1 +1,6 @@
 import random
+
+suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+
+deck = [(rank, suit) for suit in suits for rank in ranks]
